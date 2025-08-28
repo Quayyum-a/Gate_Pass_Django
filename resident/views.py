717078e93c8c251ecae5d0ed9874095d0b6d0e73
Quayyum <quayyumariyo@gmail.com>
2025-08-28 12:21:29 +0100

    feat(resident): add authentication requirement for adding house- Import IsAuthenticated permission from rest_framework.permissions- Add @permission_classes([IsAuthenticated]) decorator to add_house view- Update import statements for better organization

--- resident/views.py
+++ resident/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-
+from rest_framework.permissions import IsAuthenticated
 from .models import User, House
 from .serializers import HouseSerializer
 
 # Create your views here.
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
+
 def add_house(request):
     serializer = HouseSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
