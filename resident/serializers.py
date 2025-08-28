bb7675708e0703b5f01c3250bdda371858847304
Quayyum <quayyumariyo@gmail.com>
2025-08-28 12:22:03 +0100

    feat(resident): implement user registration and house serializers- Add UserCreateSerializer for user registration- Update HouseSerializer to use House model- Import necessary modules and classes

--- resident/serializers.py
+++ resident/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from resident.models import House, User
+
 
 class HouseSerializer(serializers.Serializer):
-    house_number = serializers.IntegerField()
-    address = serializers.CharField(max_length=255)
-    user = serializers.IntegerField()
+    class Meta:
+        model = House
+        fields = [ 'house_number', 'address']
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['username','first_name', 'last_name', 'email', 'phone', 'password']
