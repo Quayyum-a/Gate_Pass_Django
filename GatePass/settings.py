72a02524e27e35365fa6cc1defe7f1977c5b085a
Quayyum <quayyumariyo@gmail.com>
2025-08-28 12:24:26 +0100

    feat(auth): implement JWT authentication and user registration- Add Djoser and JWT authentication settings- Configure REST framework and JWT token lifetime- Update installed apps to include Djoser- Set custom user model for authentication

--- GatePass/settings.py
+++ GatePass/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 import os
 import dotenv
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'resident',
+    'djoser',
 
 ]
 
@@ -135,3 +136,18 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'resident.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'resident.serializers.UserCreateSerializer',
+
+}
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+}
